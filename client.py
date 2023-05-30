546f8475fe4196487673bcfcc328515c57b25854
RossiEduardo <eduardorossi80@hotmail.com>
2023-05-29 21:36:37 -0300

    Adicionando alguns comentarios

--- client.py
+++ client.py
@@ -12,12 +12,16 @@ class Client(DatagramProtocol):
         self.server = '127.0.0.1',9999
         print("Working on id:",self.id)
 
+    # Quando o cliente eh iniciado, startamos o nosso protocolo e dizemos pro server que o cliente
+    #esta "ready"
     def startProtocol(self):
-        self.transport.write("ready".encode("utf-8"),self.server)
+        self.transport.write("ready".encode("utf-8"), self.server)
 
+    # O cliente recebe o datagram do server com todos os outros clientes
     def datagramReceived(self, datagram, addr):
         datagram = datagram.decode('utf-8')
 
+        # o cliente deve escolher um server para se conectar
         if addr == self.server:
             print("Choose a client from these\n", datagram)
             self.address = input("Write host:"),int(input("Write port:"))
@@ -30,6 +34,6 @@ class Client(DatagramProtocol):
             self.transport.write(input(":::").encode('utf-8'), self.address)
 
 if __name__ == '__main__':
-    port = randint(1000,5000)
+    port = randint(1000,5000) #esse random eh necessario para que nao tenhamos dois clientes numa mesma porta
     reactor.listenUDP(port, Client('localhost',port))
     reactor.run()
--- server.py
+++ server.py
@@ -5,11 +5,14 @@ class Server(DatagramProtocol):
     def __init__(self):
         self.clients = set()
 
+    # O server receve o datagrama do cliente
     def datagramReceived(self, datagram, addr):
         datagram = datagram.decode("utf-8")
+        # se o server recebe o ready do nosso cliente, todos os outros clientes sao adicionados em addresses
         if datagram == "ready":
             addresses = "\n".join([str(x) for x in self.clients])
 
+            # os outros clientes serao enviados para o nosso cliente
             self.transport.write(addresses.encode('utf-8'),addr)
             self.clients.add(addr)
 
